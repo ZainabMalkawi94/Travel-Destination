aa1cab24714866923dd18d1f0ba53f3b8a666839
ZainabMalkawi94 <zainab.m.malkawi@gmail.com>
2023-04-19 10:51:34 +0000

    adding New branch called class-11

---
+++ tourapp/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Home from "./components/home/Home.js"
+// import Tours from "./components/tours/Tours.js"
+
+
+// const data = require ('../src/data/db.json')
+
+export default function App() {
+  return (
+<>
+
+<Home/>
+</>
+  )
+}
---
+++ tourapp/src/components/header/Header.js
@@ -0,0 +1,9 @@
+import './Header.css'
+
+function Header(){
+    return(
+        <h1> Tour App </h1>
+
+    )
+}
+export default Header;
